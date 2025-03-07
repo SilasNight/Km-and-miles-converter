6eb47b9541502dbe45f1719995939f14d86957b2
SilasNight <15emiliomurray@gmail.com>
2025-03-07 09:21:46 +0200

    Added distance label and distance entry and added padding to the window

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ class Converter:
         combo_list = ["Km", "Miles"]
         window = tk.Tk()
         window.title("Let's convert")
+        window.config(padx=20, pady=20)
+        window.update()
         self.distance_type_label = tk.Label(window,text="What is the distance type:")
         self.distance_type_combo = ttk.Combobox(window)
         self.distance_type_combo["values"] = combo_list
+        self.distance_label = tk.Label(window, text="How far is the distance?")
+        self.distance_entry = tk.Entry(window, )
+
 
         self.distance_type_label.grid(column=0, row=0)
-        self.distance_type_combo.grid(column=2, row=0)
+        self.distance_type_combo.grid(column=1, row=0)
+        self.distance_label.grid(column=0, row=1)
+        self.distance_entry.grid(column=1, row=1)
 
 
 
